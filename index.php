bfb9ddac972e4288715369ff1c4b02bf9eda5926
Walter Newsome <waltern@vt.edu>
2020-10-06 20:09:27 -0400

    improved title and added css file

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 $current_video = $_GET["q"];
+$video_directory = "../video/";
 $n = false;
-foreach (glob("../video/*".$current_video."*") as $filename) {
+foreach (glob($video_directory."*".$current_video."*") as $filename) {
 	$n = true;
 	$current_video_file = $filename;
 }
@@ -9,52 +10,10 @@ foreach (glob("../video/*".$current_video."*") as $filename) {
 <!DOCTYPE html> 
 <html> 
 <head>
-  <title>DRAGON BALL Z</title>
-  <link rel="icon" href="demo_icon.gif" type="image/gif" sizes="128x128">
+	<title>DRAGON BALL Z</title>
+	<link rel="icon" href="demo_icon.gif" type="image/gif" sizes="128x128">
+	<link rel="stylesheet" href="video-player-style.css">
 </head>
-<style>
-div{
-	border: 3px;
-	width: 848px;
-	background-color: #66a3ff;
-	border-radius: 10px;
-	padding: 3px;
-	margin-top: 10px;
-	opacity: 0.8;
-	margin-bottom: 10px;
-}
-a:link {
-  color: black;
-  text-decoration: none;
-}
-a:visited {
-	color: black;
-	background-color: transparent;
-	text-decoration: none;
-}
-body {
-	background-image: url("background4.jpg");
-	font-family: Arial, Helvetica, sans-serif;
-	background-repeat: no-repeat;
-	background-size: cover;
-	color: white;
-	background-color: black;
-	margin: 0px;
-}
-#controles{
-	margin-top: 5px;
-}
-#boton{
-	background-color: pink;
-	width: 180px;
-	padding: 5px;
-	display: inline-block;
-}
-#boton:hover, #boton:active{
-	background-color: blue;
-	color: white;
-}
-</style>
 <body> 
 <center>
 <?php
@@ -62,7 +21,14 @@ if($n == true){
 ?>
 
 <div>
-	<h2><?php echo $current_video_file; ?></h2>
+	<h2>
+	<?php 
+		$main_title = str_replace($video_directory, "", $current_video_file);
+		$main_title = str_replace(".mkv", "", $main_title);
+		$main_title = str_replace(".mp4", "", $main_title);
+		echo $main_title; 
+	?>
+	</h2>
 </div>
 <video width="80%" controls autoplay>
   	<source src="<?php echo $current_video_file; ?>" type="video/mp4">
